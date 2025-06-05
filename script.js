16e3a7f371bf8ae74cd82621465023633e415d39
melouwen <migalenkodenis@gmail.com>
2025-06-05 16:55:06 +0300

    2

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ async function renderCars() {
         const prev = document.createElement("div");
         prev.id = "prevCar";
         prev.className = "carousel-arrow";
-        prev.innerHTML = `<img src="media/icons/right.png" style="transform: rotate(180deg); width: 24px; height: 24px;" alt="Назад">`;
+        prev.innerHTML = `<img src="media/icons/free-icon-right-arrow-271228.png" style="transform: rotate(180deg); width: 36px; height: 36px;" alt="Назад">`;
         prev.onclick = () => {
             index = (index - 1 + cars.length) % cars.length;
             track.style.transform = `translateX(-${index * 100}%)`;
@@ -94,7 +94,7 @@ async function renderCars() {
         const next = document.createElement("div");
         next.id = "nextCar";
         next.className = "carousel-arrow";
-        next.innerHTML = `<img src="media/icons/right.png" style="width: 24px; height: 24px;" alt="Вперед">`;
+        next.innerHTML = `<img src="media/icons/free-icon-right-arrow-271228.png" style="width: 36px; height: 36px;" alt="Вперед">`;
         next.onclick = () => {
             index = (index + 1) % cars.length;
             track.style.transform = `translateX(-${index * 100}%)`;
