689188a7a297bd2c206c3d5556b1ebe64b6e5454
melouwen <migalenkodenis@gmail.com>
2025-06-06 19:50:29 +0300

    6.3

---
+++ create-admin.js
@@ -0,0 +1,15 @@
+// create-admin.js
+const bcrypt = require('bcrypt');
+const pool = require('./db');
+
+async function createAdmin(username, password) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await pool.query(
+        'INSERT INTO admins (username, password) VALUES ($1, $2)',
+        [username, hashedPassword]
+    );
+    console.log(`✅ Адмін "${username}" доданий!`);
+    process.exit();
+}
+
+createAdmin("admin", "1234");
